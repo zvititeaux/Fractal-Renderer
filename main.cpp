dc1294344b6aff74e851ee08e4d2cef9bda50a65
zvititeaux <zvititeaux@gmail.com>
2023-10-06 01:16:37 -0400

    Main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
 
 const int WIDTH = 800; // Set window width
-const int HEIGHT = 600; // Set window height  
+const int HEIGHT = 600; // Set window height 
+const int WIDTH = 800;
