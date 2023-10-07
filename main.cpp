ede8dd93dc862f43eabd57d805b8aa82db86a718
zvititeaux <zvititeaux@gmail.com>
2023-10-06 23:23:41 -0400

    Current version, generates mandelbrot set in grayscale.

--- main.cpp
+++ main.cpp
@@ -2,4 +2,60 @@
 
 const int WIDTH = 800; // Set window width
 const int HEIGHT = 600; // Set window height 
-const int WIDTH = 800;
+
+int mandelbrot(double x0, double y0, int max_iteration) {
+	double x = 0.0;
+	double y = 0.0;
+	int iteration = 0;
+	while (x * x + y * y <= 2 * 2 && iteration < max_iteration) {
+		double xtemp = x * x - y * y + x0;
+		y = 2 * x * y + y0;
+		x = xtemp;
+		iteration++;
+	}
+	return iteration;
+}
+
+int main() {
+	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Mandelbrot Set");
+
+	sf::Image image;
+	image.create(WIDTH, HEIGHT, sf::Color::Black);
+
+	for (int Px = 0; Px < WIDTH; Px++) {
+		for (int Py = 0; Py < HEIGHT; Py++) {
+			double x0 = (double)Px / WIDTH * (0.47 + 2.00) - 2.00;
+			double y0 = (double)Py / HEIGHT * (1.12 + 1.12) - 1.12;
+
+			int value = mandelbrot(x0, y0, 1000);
+
+			// This part colors the pixel. Adjusts this for your needs:
+			sf::Color color(255 - value % 255, 255 - value % 255, 255 - value % 255);
+			if (value == 1000) {
+				color = sf::Color::Black; 
+			}
+			image.setPixel(Px, Py, color);
+		}
+	}
+
+	sf::Texture texture;
+	texture.loadFromImage(image);
+
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
+
+	while (window.isOpen()) {
+		sf::Event event;
+		while (window.pollEvent(event)) {
+			if (event.type == sf::Event::Closed) {
+				window.close();
+			}
+		}
+		window.clear();
+		window.draw(sprite);
+		window.display();
+	}
+
+	return 0;
+}
+
